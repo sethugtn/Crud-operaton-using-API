f4187693e566f42bdc29aedcb300ef906281d883
sethugtn <sethugtn@gmail.com>
2023-12-24 15:00:57 +0530

    E-Kart Dashboard Project

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UsersComponent } from './component/users/users.component';
+import { UserdetailComponent } from './component/userdetail/userdetail.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UsersComponent,
+    UserdetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
